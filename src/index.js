e24e45951b68aa4e23cffe5c0ae3cb68d3f55ca2
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-11-28 13:28:58 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
+
 dotenv.config({
   path: "./env",
 });
+
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
