62741269972fadbc197389a11ff6f7ecb71b49fa
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-28 16:14:09 +0530

    format+

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,9 +1,8 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next))
-        .catch((err) => next(err))
-    }
-}
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
 // const asyncHandler = (fn) => {
 //     () => {
@@ -11,4 +10,15 @@ const asyncHandler = (requestHandler) => {
 //     }
 // }
 
-export {asyncHandler}
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+export { asyncHandler };
