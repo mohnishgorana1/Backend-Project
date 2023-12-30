d464a176bb1fab9fab8d327ff626b71623a309aa
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-30 23:23:13 +0530

    add some routes and debug

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,10 @@ import {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middeleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,6 +33,13 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route('/refresh-token').post(refreshAccessToken)
+router.route('/current-user').get(verifyJWT, getCurrentUser)
+router.route('/update-account').patch(verifyJWT, updateAccountDetails)
+
+router.route('/avatar').patch(verifyJWT, upload.single('avatar'), updateUserAvatar)
+router.route('/cover-image').patch(verifyJWT, upload.single('/coverImage', updateUserCoverImage))
+
+
 
 
 export default router;
